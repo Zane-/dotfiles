2ded10308942d0de7a91aeeeac9d36b7a5ecebe5
Zane Bilous <zanebilous@gmail.com>
2018-03-06 19:38:23 -0500

    Update setup script

--- setup.sh
+++ setup.sh
@@ -5,32 +5,29 @@ mkdir -p ~/dotfiles/backups
 
 # create symbolic links
 echo "[+] Creating symbolic links, moving current dotfiles to ~/dotfiles/backups"
-for dotfile in .vimrc .zshrc .zpreztorc
-do
+for dotfile in .vimrc .zshrc .zpreztorc; do
 	if [ -f ~/$dotfile ]; then
-		mv ~/$dotfile ~/dotfiles/backups
+		mv --backup=t ~/$dotfile ~/dotfiles/backups
 	fi
 	ln -s ~/dotfiles/$dotfile ~/$dotfile
 	echo "[+] Created $dotfile"
 done
 
-# install vundle
-if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
-	echo "[+] Installing Vundle"
-	if [ -x "$(command -v git)" ]; then
-		git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-		vim +PluginInstall +qall
-	else
-		echo "[-] Git is not installed, install with sudo apt-get install git"
-	fi
+# install plugged
+if [ ! -d ~/.vim/plugged ]; then
+	 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+		 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
+
+# install vim plugins
+vim +PlugInstall +qall
 
 # make vim undodir
 mkdir -p ~/.vim/undodir
 
 # link ycm config
 if [ -f ~/.vim/.ycm_extra_conf.py ]; then
-	mv ~/.vim/.ycm_extra_conf.py ~/dotfiles/backups
+	mv --backup=t ~/.vim/.ycm_extra_conf.py ~/dotfiles/backups
 fi
 ln -s ~/dotfiles/.ycm_extra_conf.py ~/.vim
 
@@ -40,6 +37,16 @@ if  [ ! -d ~/.zprezto ]; then
 	if [ -x "$(command -v git)" ]; then
 		git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 	else
-		echo "Git is not installed, install with sudo apt-get install git"
+		echo "[-] Git is not installed, install with sudo apt-get install git"
 	fi
 fi
+
+# copy over prezto dotfiles
+for dotfile in zlogin zlogout zprofile zshenv; do
+	if [ -f ~/.$dotfile ]; then
+		mv --backup=t ~/.$dotfile ~/dotfiles/backups
+	fi
+	ln -s ~/.zprezto/runcoms/$dotfile ~/.$dotfile
+done
+
+echo "[+] Dotfiles installed. Your old dotfiles have been placed in ~/dotfiles/backups"

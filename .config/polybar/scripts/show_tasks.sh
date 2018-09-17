eef2b748ead1bc7badf257138e3d1401ef6ae74e
Zane Bilous <zanebilous@gmail.com>
2018-09-17 14:22:34 -0400

    Update script

--- .config/polybar/scripts/show_tasks.sh
+++ .config/polybar/scripts/show_tasks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rofi=`echo -e "$(task list)" | rofi -dmenu -lines 10 -font "Roboto Mono for Powerline 9" -hide-scrollbar -columns 1 -width 35 -location 1 -xoffset 202 -yoffset 56 -dpi 180 -p tasks -color-window '#ff282c34, #00000000, #00000000' -color-normal '#ff282c34, #99666666, #ff282c34, #22ffffff, #99e5c07b' | awk '{print}'`
+rofi=`echo -e "$(task list | awk '{if(NR>3)print}')" | rofi -dmenu -lines 10 -font "Roboto Mono for Powerline 9" -hide-scrollbar -columns 1 -width 35 -location 1 -xoffset 202 -yoffset 56 -dpi 180 -p tasks -color-window '#ff282c34, #00000000, #00000000' -color-normal '#ff282c34, #99666666, #ff282c34, #22ffffff, #99e5c07b' | awk '{print}'`
 
 echo $rofi
 

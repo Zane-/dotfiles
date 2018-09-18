62f7394cdcc8814ca92155d67ff66888f5bf5bbf
Zane Bilous <zanebilous@gmail.com>
2018-09-17 20:09:47 -0400

    Update lightbar menu

--- scripts/lightbar.sh
+++ scripts/lightbar.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-OPTIONS="\tdefault\n\tcolor\n\trainbow\n\tsiren\n\tpulse\n\t"
-ROFI=`echo $OPTIONS | rofi -dmenu -i -lines 5 -font "Roboto Mono for Powerline 9" -columns 1 -width 15 -dpi 180 -p light -color-window '#ff282c34, #00000000, #00000000' -color-normal '#ff282c34, #99666666, #ff282c34, #22ffffff, #99e5c07b' | awk '{print $1}'`
+OPTIONS="\tdefault\n\tcolor\n\trainbow\n\tgradient\n\tsiren\n\tpulse"
+ROFI=`echo $OPTIONS | rofi -dmenu -i -lines 6 -font "Roboto Mono for Powerline 9" -columns 1 -width 15 -dpi 180 -p light -color-window '#ff282c34, #00000000, #00000000' -color-normal '#ff282c34, #99666666, #ff282c34, #22ffffff, #99e5c07b' | awk '{print $1}'`
 
 echo $ROFI
 
 if [ ${#ROFI} -gt 0 ]; then
-	sudo kill $(pgrep '(rainbow|siren|pulse).sh')
+	sudo kill $(pgrep '(rainbow|siren|pulse|gradient).sh')
     case $ROFI in
     default)
         sudo ~/lightbar_scripts/default.sh
@@ -17,6 +17,9 @@ if [ ${#ROFI} -gt 0 ]; then
     rainbow)
         sudo ~/lightbar_scripts/rainbow.sh
         ;;
+	gradient)
+		sudo ~/lightbar_scripts/gradient.sh
+		;;
     siren)
         sudo ~/lightbar_scripts/siren.sh
         ;;

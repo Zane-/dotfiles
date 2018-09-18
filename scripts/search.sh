de34c63adef4d4e53f5ca2d559e3a43e7b68b26c
Zane Bilous <zanebilous@gmail.com>
2018-09-18 16:16:35 -0400

    Update prompt text

--- scripts/search.sh
+++ scripts/search.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rofi=`rofi -dmenu -i -lines 0  -font "Roboto Mono for Powerline 11" -hide-scrollbar -columns 1 -width 30 -location 0 -dpi 200 -p search -color-window '#ff282c34, #00000000, #00000000' -color-normal '#ff282c34, #99666666, #ff282c34, #22ffffff, #99e5c07b' | awk '{print}'`
+rofi=`rofi -dmenu -i -lines 0  -font "Material Design Icons 10" -hide-scrollbar -columns 1 -width 30 -location 0 -dpi 220 -p  -color-window '#ff282c34, #00000000, #00000000' -color-normal '#ff282c34, #99666666, #ff282c34, #22ffffff, #99e5c07b' | awk '{print}'`
 
 if [[ -n $rofi ]]; then
 	xdg-open "https://www.google.com/search?q=$rofi"
--- scripts/shell.sh
+++ scripts/shell.sh
@@ -2,6 +2,8 @@
 
 cd ~
 
-rofi=`rofi -dmenu -i -lines 0  -font "Roboto Mono for Powerline 9" -hide-scrollbar -columns 1 -width 33 -location 3 -yoffset 56 -dpi 180 -p shell -color-window '#ff282c34, #00000000, #00000000' -color-normal '#ff282c34, #99666666, #ff282c34, #22ffffff, #99e5c07b' | awk '{print}'`
+rofi=`rofi -dmenu -i -lines 0  -font "Material Design Icons 9" -hide-scrollbar -columns 1 -width 20 -location 2 -yoffset 56 -dpi 220 -p  -color-window '#ff282c34, #00000000, #00000000' -color-normal '#ff282c34, #99666666, #ff282c34, #22ffffff, #99e5c07b' | awk '{print}'`
 
-exec $rofi
+if [[ -n $rofi ]]; then
+	exec $rofi
+fi

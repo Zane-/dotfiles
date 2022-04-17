e08fde54720dff941f9dee1d54af1fc21c2a27ca
Zane <zanebilous@gmail.com>
2022-04-17 16:35:24 -0700

    Add option to install rust on WSL to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # make backup directory
 mkdir -p ~/dotfiles/backups
 # make directory for global packages (for user)
@@ -32,6 +31,11 @@ read -p "[+] Install dependencie and tools? (y/n): " opt
 if [ $opt == "y" ]; then
 	sudo apt-get update
 	sudo apt-get install -y bat build-essential libssl-dev libreadline-dev libsqlite3-dev silversearcher-ag exuberant-ctags clang clang-format cmake default-jdk golang python3-dev nodejs npm ruby ruby-dev tldr aria2c git fasd exa
+fi
+
+read -p "[+] Are you on WSL and wanting to install Rust? (y/n): " opt2
+if [ $opt2 ==  "y" ]; then
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
 
 # install vim plugins

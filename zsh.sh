ffefc34c0bc8f09c6bab41f7a2540350ab4cf5d7
Zane <zanebilous@gmail.com>
2022-05-04 20:20:56 -0700

    Fix zprezto fzf installation

--- zsh.sh
+++ zsh.sh
@@ -23,7 +23,7 @@ if  [ ! -d ~/.zprezto ]; then
 	git clone --recursive https://github.com/sorin-ionescu/prezto.git "$HOME/.zprezto"
 
 	mkdir -p $HOME/.zprezto/contrib
-	git submodule add -f https://github.com/lildude/fzf-prezto $HOME/.zprezto/contrib/fzf
+	cd $HOME/.zprezto && git submodule add -f https://github.com/lildude/fzf-prezto $HOME/.zprezto/contrib/fzf
 fi
 
 echo "[+] Creating symbolic links..."

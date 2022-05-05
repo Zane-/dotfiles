4273389abf5f9335d432ff5e55fc9d1ca9dcf56e
Zane <zanebilous@gmail.com>
2022-05-04 19:55:35 -0700

    Install PowerlineSymbols.otf

--- tmux.sh
+++ tmux.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# install powerline font first
+if ! fc-list | grep "PowerlineSymbols"; then
+	mkdir -p ~/.local/share/fonts
+	mkdir -p ~/.config/fontconfig/conf.d
+	wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf -P ~/.local/share/fonts
+	wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf -P ~/.config/fontconfig/conf.d
+	fc-cache -vf ~/.local/share/fonts
+
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 mkdir -p ~/dotfiles/backups

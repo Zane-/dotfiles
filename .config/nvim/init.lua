a82e118ba73bd1bc46121cde2e784ece90986c1e
Zane <zanebilous@gmail.com>
2022-05-21 00:42:55 -0700

    Switch back to nvim-cmp for completion

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -193,23 +193,27 @@ require('packer').startup(function()
 	use 'akinsho/bufferline.nvim' -- nice buffer line
 	use 'folke/tokyonight.nvim' -- colorscheme
 	use 'folke/which-key.nvim' -- shortcut popup
-	use 'glepnir/dashboard-nvim' -- fancy start page
 	use 'folke/trouble.nvim' -- aesthetic diagnostics page
 	use 'FeiyouG/command_center.nvim' -- command palette
+	use 'glepnir/dashboard-nvim' -- fancy start page
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/cmp-cmdline'
+	use 'hrsh7th/cmp-nvim-lsp' -- completion stuff
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/nvim-cmp'
 	use 'kosayoda/nvim-lightbulb' -- show a lightbulb for code actions
 	use 'kyazdani42/nvim-tree.lua' -- filetree
 	use 'kyazdani42/nvim-web-devicons' -- file icons
+	use 'L3MON4D3/LuaSnip' -- snippets
 	use 'lewis6991/gitsigns.nvim' -- git integration
 	use 'markonm/traces.vim' -- live preview for substitution
 	use { 'michaelb/sniprun', run = 'bash ./install.sh' } -- run code snippets
 	use 'mfussenegger/nvim-dap' -- debugger
-	use 'ms-jpq/coq_nvim' -- completion
-	use 'ms-jpq/coq.artifacts' -- snippets
 	use 'neovim/nvim-lspconfig' -- completion, go-to, etc.
 	use 'numToStr/Comment.nvim' -- comments
 	use 'numToStr/FTerm.nvim' -- terminal popup
-	use 'nvim-lua/plenary.nvim'
-	use 'nvim-lua/popup.nvim'
+	use 'nvim-lua/plenary.nvim' -- UI dependency
+	use 'nvim-lua/popup.nvim' -- UI dependency
 	use 'nvim-lualine/lualine.nvim' -- status line
 	use 'nvim-telescope/telescope.nvim' -- aesthetic fuzzyfinder
 	use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
@@ -219,6 +223,7 @@ require('packer').startup(function()
 	use 'olimorris/onedarkpro.nvim' -- colorscheme
 	use 'phaazon/hop.nvim' -- easy navigation
 	use 'stevearc/dressing.nvim' -- use telescope for more things
+	use 'rafamadriz/friendly-snippets' -- snippets
 	use 'rmagatti/auto-session' -- sessions based on cwd
 	use 'roxma/vim-paste-easy' -- auto-enter paste mode on paste
 	use 'RRethy/vim-illuminate' -- highlight symbol under cursor
@@ -281,11 +286,6 @@ require("bufferline").setup {}
 require('Comment').setup()
 
 ----------------------------------
---         coq config
-----------------------------------
-cmd [[ let g:coq_settings = { 'auto_start': 'shut-up' } ]]
-
-----------------------------------
 --       Dashboard config
 ----------------------------------
 g.dashboard_default_executive = 'telescope'
@@ -306,14 +306,16 @@ require('hop').setup({ keys = 'etovxqpdygfblzhckisuran' })
 require('goto-preview').setup {
 	opacity = 7
 }
-local coq = require('coq')
+
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 local lsp_installer = require('nvim-lsp-installer')
 
 lsp_installer.on_server_ready(function(server)
 	local opts = {
+		capabilities = capabilities,
 		on_attach = on_attach,
 	}
-	server:setup(coq.lsp_ensure_capabilities(opts))
+	server:setup(opts)
 end)
 
 ----------------------------------
@@ -528,6 +530,124 @@ lualine.setup(config)
 --     nvim-autopairs config
 ----------------------------------
 require('nvim-autopairs').setup {}
+
+----------------------------------
+--        nvim-cmp config
+----------------------------------
+require("luasnip.loaders.from_vscode").lazy_load()
+
+local luasnip = require('luasnip')
+local cmp = require('cmp')
+
+local kind_icons = {
+	Text = '',
+	Method = '',
+	Function = '',
+	Constructor = '',
+	Field = '',
+	Variable = '',
+	Class = 'ﴯ',
+	Interface = '',
+	Module = '',
+	Property = 'ﰠ',
+	Unit = '',
+	Value = '',
+	Enum = '',
+	Keyword = '',
+	Snippet = '',
+	Color = '',
+	File = '',
+	Reference = '',
+	Folder = '',
+	EnumMember = '',
+	Constant = '',
+	Struct = '',
+	Event = '',
+	Operator = '',
+	TypeParameter = ''
+}
+
+cmp.setup({
+	enabled = function()
+		-- disable completion in comments
+		local context = require 'cmp.config.context'
+		-- keep command mode completion enabled when cursor is in a comment
+		if vim.api.nvim_get_mode().mode == 'c' then
+			return true
+		else
+			return not context.in_treesitter_capture("comment")
+					and not context.in_syntax_group("Comment")
+		end
+	end,
+	formatting = {
+		format = function(entry, vim_item)
+			vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind)
+			return vim_item
+		end
+	},
+	snippet = {
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body)
+		end,
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-b>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then cmp.select_next_item()
+			elseif luasnip.expand_or_locally_jumpable() then luasnip.expand_or_jump()
+			else fallback()
+			end
+		end
+			, { 'i', 'c' }),
+		['<S-Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then luasnip.jump(-1)
+			else fallback()
+			end
+		end
+			, { 'i', 'c' }),
+		['<cr>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		{ name = 'luasnip' },
+	}, {
+		{ name = 'buffer' },
+	})
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+	sources = cmp.config.sources({
+		{ name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+	}, {
+		{ name = 'buffer' },
+	})
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = 'buffer' }
+	}
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = 'path' }
+	}, {
+		{ name = 'cmdline' }
+	})
+})
 
 ----------------------------------
 --       nvim-tree config
@@ -1051,7 +1171,6 @@ wk.register({
 	},
 	[';'] = 'Input command',
 	['\\'] = 'Search through all files',
-	['<c-h>'] = 'Navigate to next mark in snippet',
 	['<c-k>'] = 'Open signature help',
 	['<c-l>'] = 'Unhighlight search results',
 	['<c-n>'] = 'Toggle file tree',

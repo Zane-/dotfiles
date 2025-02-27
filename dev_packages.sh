c7aec58fb78837d6e30349ebf77002a352c20544
Zane Bilous <zanebilous@gmail.com>
2025-02-27 15:57:11 -0800

    Add fd-find dep

--- dev_packages.sh
+++ dev_packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [ ! -x "$(command -v apt-get)" ]; then
-	sudo apt-get update && sudo apt-get install -y bat build-essential libssl-dev libreadline-dev libsqlite3-dev clang clang-format cmake default-jdk golang python3-dev nodejs npm ruby ruby-dev aria2 git fasd exa fzf ripgrep
+	sudo apt-get update && sudo apt-get install -y bat build-essential libssl-dev libreadline-dev libsqlite3-dev clang clang-format cmake default-jdk golang python3-dev nodejs npm ruby ruby-dev aria2 git fasd exa fzf ripgrep fd-find
 fi
